bb396d1714725cb6a13692f0f7dfb441a9a046c3
mrmuhdamir <whiteforestcat96@gmail.com>
2023-03-01 11:15:24 +0800

    table SQL commands

--- BackEnd-postgres/sql/database.sql
+++ BackEnd-postgres/sql/database.sql
@@ -6,3 +6,22 @@ CREATE TABLE user_accounts(
     hash VARCHAR(255) NOT NULL,
     admin BOOLEAN
 );
+
+CREATE TABLE item_gallery(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(50) NOT NULL,
+    tags VARCHAR(50) NOT NULL,
+    stock SMALLINT NOT NULL
+);
+
+CREATE TABLE user_cart(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    quantity SMALLINT NOT NULL,
+);
+
+CREATE TABLE shipping(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    order_date DATE NOT NULL,
+    delivery_date DATE NOT NULL,
+);
