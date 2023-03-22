4c7da35c1d285049f452670f22d913680d95b805
mrmuhdamir <whiteforestcat96@gmail.com>
2023-03-22 12:56:04 +0800

    rearrrange files for deployment

--- BackEndV2/models/seed.js
+++
@@ -1,8 +0,0 @@
-let seedUsers = [
-  { email: "mrmuhdamir@gmail.com", password: "92344590", admin: true },
-  { email: "test@gmail.com", password: "123456", admin: false },
-  { email: "example@gmail.com", password: "123456", admin: false },
-  { email: "hello@gmail.com", password: "123456", admin: false },
-];
-
-module.exports = seedUsers;
