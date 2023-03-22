bbd6c3625bab3b6115b867756c3a36f61c8df2dc
mrmuhdamir <whiteforestcat96@gmail.com>
2023-03-22 12:22:29 +0800

    update db for deployment

--- BackEnd-postgres/db/db.js
+++ BackEnd-postgres/db/db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   password: "92344590",
-  host: "localhost",
+  host: "my-db-instance.chh5epswnlll.ap-southeast-2.rds.amazonaws.com",
   port: 5432,
   database: "ecommerce",
 });
